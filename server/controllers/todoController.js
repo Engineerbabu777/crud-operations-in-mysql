31090bb847a83cb15496ec67a11a2eeaab3e1c67
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-02-14 13:59:34 +0500

    added-table-creation

--- server/controllers/todoController.js
+++ server/controllers/todoController.js
@@ -11,3 +11,14 @@ exports.createDB = (req,res) => {
       return res.status(201).json("DB CREATED!")  
     })
 }
+
+
+// CREATE TABLE!
+exports.createTable = (req,res) => {
+    let q = 'CREATE TABLE todos(id int AUTO_INCREMENT, firstName VARCHAR(255), lastName VARCHAR(255), email VARCHAR(255), password VARCHAR(255), PRIMARY KEY(id))';
+
+    db.query(q,(err,result) => {
+      if (err) throw err;
+      return res.status(201).json("TABLE CREATED!")  
+    })
+}
--- server/routes/todoRoutes.js
+++ server/routes/todoRoutes.js
@@ -1,11 +1,11 @@
 
 const express = require("express");
-const { createDB } = require("../controllers/todoController");
+const { createDB,createTable } = require("../controllers/todoController");
 
 const router = express.Router();
 
 
 // ROUTES!
 router.get("/create/database", createDB);
-
+router.get("/create/table", createTable);
 module.exports = router;
